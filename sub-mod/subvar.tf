9cd7345b3b2775c2070ae4f7c5b8db74bfee2248
Riv66 <160268092+Riv66@users.noreply.github.com>
2024-10-02 22:42:52 +0100

    Create subvar.tf

---
+++ sub-mod/subvar.tf
@@ -0,0 +1,14 @@
+variable "mod-keyp" {
+  description = "Var passed from root"
+  type        = string
+}
+
+variable "mod-pubname" {
+  description = "Var passed from root"
+  type        = string
+}
+
+variable "mod-privname" {
+  description = "Var passed from root"
+  type        = string
+}
