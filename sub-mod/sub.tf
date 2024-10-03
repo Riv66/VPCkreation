5fb5a715aea344074f5a1ffc2622446388fc3206
Riv66 <160268092+Riv66@users.noreply.github.com>
2024-10-03 11:33:43 +0100

    data update

--- sub-mod/sub.tf
+++ sub-mod/sub.tf
@@ -3,7 +3,7 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-data "aws_vpc" "vpclist" {
+data "aws_vpc" "vpcl" {
     tags = {
     owner   = var.mod-tags["owner"]
    }
@@ -14,7 +14,7 @@ data "aws_vpc" "vpclist" {
 resource "aws_subnet" "pub" {
   for_each                                    = var.mod-pubsub # Name - CDIR map
   cidr_block                                  = each.value
-  vpc_id                                      = aws_vpc.vpclist.id
+  vpc_id                                      = aws_vpc.vpcl.vpc_id
   map_public_ip_on_launch                     = true
   availability_zone                           = data.aws_availability_zones.available.names[0]
   enable_resource_name_dns_a_record_on_launch = true
@@ -29,7 +29,7 @@ resource "aws_subnet" "pub" {
 # Create private subnets in the same available availability zone
 resource "aws_subnet" "pri" {
   for_each                = var.mod-prisub # Name - CDIR map
-  vpc_id                  = aws_vpc.vpclist.id
+  vpc_id                  = aws_vpc.vpcl.vpc_id
   cidr_block              = each.value
   map_public_ip_on_launch = false
   availability_zone       = data.aws_availability_zones.available.names[0]
