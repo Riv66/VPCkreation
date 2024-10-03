f9588ebd58851d9dd1f6b9a2771e6e1a843427f3
Riv66 <160268092+Riv66@users.noreply.github.com>
2024-10-03 16:51:25 +0100

    data

---
+++ sub-mod/sub-data.tf
@@ -0,0 +1,25 @@
+# Declare the data sources
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "aws_vpc" "vpclist" {
+
+# to test input to filter
+  filter {
+    name   = "cidr-block"
+    values = ["${var.mod-cdir}"]
+  }
+
+# to test filter
+  filter {
+    name   = "tag:Name"
+    values = ["${var.mod-vpcname}"]
+  }
+
+  # filter tested ok as option
+ filter {
+    name   = "isDefault"
+    values = ["false"]
+  }
+}
