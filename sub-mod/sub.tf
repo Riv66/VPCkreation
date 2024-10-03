8185ad7eb35f695c1a6ea7ffff6590df69be5f37
Riv66 <160268092+Riv66@users.noreply.github.com>
2024-10-03 11:44:45 +0100

    data fix3

--- sub-mod/sub.tf
+++ sub-mod/sub.tf
@@ -14,7 +14,7 @@ data "aws_vpc" "vpclist" {
 resource "aws_subnet" "pub" {
   for_each                                    = var.mod-pubsub # Name - CDIR map
   cidr_block                                  = each.value
-  vpc_id                                      = data.aws_vpc.vpclist.vpc_id
+  vpc_id                                      = data.aws_vpc.vpclist.vpc_id[0]
   map_public_ip_on_launch                     = true
   availability_zone                           = data.aws_availability_zones.available.names[0]
   enable_resource_name_dns_a_record_on_launch = true
@@ -29,7 +29,7 @@ resource "aws_subnet" "pub" {
 # Create private subnets in the same available availability zone
 resource "aws_subnet" "pri" {
   for_each                = var.mod-prisub # Name - CDIR map
-  vpc_id                  = data.aws_vpc.vpclist.vpc_id
+  vpc_id                  = data.aws_vpc.vpclist.vpc_id[0]
   cidr_block              = each.value
   map_public_ip_on_launch = false
   availability_zone       = data.aws_availability_zones.available.names[0]
