d0915960322b874bd7a836889f684a5a35d571e9
Riv66 <160268092+Riv66@users.noreply.github.com>
2024-10-03 19:23:30 +0100

    up

--- sub-mod/rt/rt.tf
+++ sub-mod/rt/rt.tf
@@ -20,11 +20,3 @@ resource "aws_route_table" "pri-rt" {
   }
 }
 
-
-#add default route to igw 
-resource "aws_route" "rtIGW" {
-  route_table_id         = aws_route_table.pub-rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
-
-}
