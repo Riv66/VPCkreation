9ae8bf96b012a8be998706a63594ec868ac41f7b
Riv66 <160268092+Riv66@users.noreply.github.com>
2024-10-03 16:15:13 +0100

    variable "mod-vpcname"

--- sub-mod/sub-var.tf
+++ sub-mod/sub-var.tf
@@ -16,3 +16,9 @@ variable "mod-tags" {
   description = "Var passed from root"
     type        = map(any)
 }
+
+variable "mod-vpcname" {
+  description = "Var passed from root"
+    type        = string
+}
+
