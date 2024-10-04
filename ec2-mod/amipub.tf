fb91c51f24a4c78934318b558b2c9ea513381429
Riv66 <160268092+Riv66@users.noreply.github.com>
2024-10-04 18:11:43 +0100

    #Create Web Server AMI

--- ec2-mod/amipub.tf
+++ ec2-mod/amipub.tf
@@ -1,10 +1,10 @@
 #Create Web Server AMI
 resource "aws_ami_from_instance" "example" {
  # name               = "MyPrivateWebServerAmi"
+ name = "${var.mod-tags["owner"]}-WebserverAMI"
   source_instance_id = aws_instance.webserver.id
      tags = {
     tier    = "Public"
-    name = "${var.mod-tags["owner"]}-WebserverAMI"
     owner   = var.mod-tags["owner"]
     service = var.mod-tags["service"]
  }
