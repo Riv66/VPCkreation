1e92a72949ed9196fffb69589066269b352f2d2e
Riv66 <160268092+Riv66@users.noreply.github.com>
2024-10-09 17:48:17 +0100

    m

--- sub-mod/sub-main.tf
+++ sub-mod/sub-main.tf
@@ -20,7 +20,8 @@ resource "aws_subnet" "pri" {
   for_each                = var.mod-prisub # map
   vpc_id                  = data.aws_vpc.vpclist.id
   #cidr_block              = each.value
-   cidr_block = cidrsubnet(data.aws_vpc.vpclist.cidr_block, 8, (index(keys(var.mod-prisub)), each.key))
+  # cidr_block = cidrsubnet(data.aws_vpc.vpclist.cidr_block, 8, (index(keys(var.mod-prisub)), each.key))
+  cidr_block = cidrsubnet(data.aws_vpc.vpclist.cidr_block, 8, (index(keys(var.prisub), each.key)))
   map_public_ip_on_launch = false
   availability_zone       = data.aws_availability_zones.available.names[0]
   tags = {
