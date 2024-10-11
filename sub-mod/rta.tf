1c89556e1a110e3b4085fd09422dfea04f7c74f8
Riv66 <160268092+Riv66@users.noreply.github.com>
2024-10-11 15:49:14 +0100

    m

--- sub-mod/rta.tf
+++ sub-mod/rta.tf
@@ -1,7 +1,7 @@
 
 #Create rt association for public subnets using for_each
 resource "aws_route_table_association" "rapub" {
-count = var.mod-public_subnet_count
+count = var.mod-public_subnet_count * var.mod-az_count
   subnet_id      = aws_subnet.pub[count.index].id 
     route_table_id = aws_route_table.pub-rt.id
   depends_on = [
@@ -11,7 +11,7 @@ count = var.mod-public_subnet_count
 
 #Create rt association for pri subnet using count 
 resource "aws_route_table_association" "rapri" {
-count = var.mod-private_subnet_count
+count = var.mod-private_subnet_count * var.mod-az_count
          subnet_id     = aws_subnet.pri[count.index].id  
   route_table_id = aws_route_table.pri-rt.id
   depends_on = [
