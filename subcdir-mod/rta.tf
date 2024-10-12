ea582d253486155ebd312b5bfb8c414677fc3e22
Riv66 <160268092+Riv66@users.noreply.github.com>
2024-10-12 12:41:25 +0100

    m

--- subcdir-mod/rta.tf
+++ subcdir-mod/rta.tf
@@ -3,7 +3,7 @@
 resource "aws_route_table_association" "rapub" {
 #count = var.mod-public_subnet_count * var.mod-az_count
  # subnet_id      = aws_subnet.pub[count.index].id 
-  for_each = aws_subnet.pub.subnet_id
+  for_each = aws_subnet.pub[each.key]
   subnet_id = each.value.id
     route_table_id = aws_route_table.pub-rt.id
   depends_on = [
@@ -15,7 +15,7 @@ resource "aws_route_table_association" "rapub" {
 resource "aws_route_table_association" "rapri" {
 #count = var.mod-private_subnet_count * var.mod-az_count
  #        subnet_id     = aws_subnet.pri[count.index].id  
-   for_each = aws_subnet.pri.subnet_id
+   for_each = aws_subnet.pri[each.key]
   subnet_id = each.value.subnet_id
     route_table_id = aws_route_table.pri-rt.id
   depends_on = [
